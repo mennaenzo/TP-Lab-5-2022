7a9000e8dd0680dbb400024cf0a819fa61620d97
mennaenzo <menna.enzo@hotmail.com>
2022-06-14 22:52:04 -0300

    first commit

---
+++ src/main/java/com/lab5/tp2022/TpLab52022Application.java
@@ -0,0 +1,13 @@
+package com.lab5.tp2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpLab52022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpLab52022Application.class, args);
+	}
+
+}
